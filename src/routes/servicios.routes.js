001f7dd5b1045c837d10e196bf3f3fe4e96104f6
Fátima Alfaro <fatialfaro99@gmail.com>
2026-07-05 23:47:18 -0300

    Agregue el controlador y la ruta para obtener un unico servicio por el id

--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { prueba } from "../controllers/servicios.controllers.js";
-import { crearServicio, listarServicios} from "../controllers/servicios.controllers.js"
+import { crearServicio, listarServicios, buscarServicioPorID} from "../controllers/servicios.controllers.js"
 
 const router = Router();
 
 //http://localhost:3000/api/servicios/test
 router.route('/test').get(prueba)
 router.route('/').post(crearServicio).get(listarServicios)
+router.route('/:id').get(buscarServicioPorID)
 export default router
