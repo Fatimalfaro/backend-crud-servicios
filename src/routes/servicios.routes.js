2448a98df56533d369a546922c16d8f5997723e2
Fátima Alfaro <fatialfaro99@gmail.com>
2026-07-05 23:18:51 -0300

    Agregue la ruta y controlador para listar todos los servicios

--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { prueba } from "../controllers/servicios.controllers.js";
-import { crearServicio} from "../controllers/servicios.controllers.js"
+import { crearServicio, listarServicios} from "../controllers/servicios.controllers.js"
 
 const router = Router();
 
 //http://localhost:3000/api/servicios/test
 router.route('/test').get(prueba)
-router.route('/').post(crearServicio)
-
+router.route('/').post(crearServicio).get(listarServicios)
 export default router
