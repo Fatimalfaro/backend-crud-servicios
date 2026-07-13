6b54bb7610506788f53c19bd58c8c7ac9915133e
Fátima Alfaro <fatialfaro99@gmail.com>
2026-07-12 22:46:43 -0300

    Dividimos las reglas de validacion para el post y put y creamos una alternativa para el patch

--- src/middlewares/validacionServicios.js
+++ src/middlewares/validacionServicios.js
@@ -2,10 +2,8 @@ import { body, param } from "express-validator";
 import resultadoValidacion from "./resultadosValidacion.js";
 
 
-export const validacionServicio = [
+const reglasServicio = [
     body('nombreServicio')
-    .notEmpty()
-    .withMessage('El nombre del servicio es un dato obligatorio')
     .isString()
     .withMessage('El nombre del servicio debe ser un string')
     .isLength({min: 5, max: 100})
@@ -13,39 +11,39 @@ export const validacionServicio = [
 
 
     body('precio')
-    .notEmpty()
-    .withMessage('El precio del servicio es un dato obligatorio')
     .isNumeric()
     .withMessage('El precio del servicio debe ser un número')
     .isFloat({min: 50})
     .withMessage('El precio del servicio debe ser un número mayor o igual a 50'),
 
     body('descripcion')
-    .notEmpty()
-    .withMessage('La descripción del servicio es un dato obligatorio')
     .isString()
     .withMessage('La descripción del servicio debe ser un string')
     .isLength({min: 10, max: 500})
     .withMessage('La descripción del servicio debe tener entre 10 y 500 caracteres'),
 
      body('imagen')
-    .notEmpty()
-    .withMessage('La imagen del servicio es un dato obligatorio')
     .isString()
     .withMessage('La imagen del servicio debe ser un string')
     .matches(/^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/)
     .withMessage('La imagen del servicio debe ser una URL válida y debe terminar con una extensión de imagen válida (jpg, jpeg, png, webp, avif, svg)'), 
     
     body('categoria')
-    .notEmpty()
-    .withMessage('La categoría del servicio es un dato obligatorio')
     .isString()
     .withMessage('La categoría del servicio debe ser un string')
     .isIn(['Desarrollo Web', 'Backend & API', 'Consultoría'])
     .withMessage('La categoría del servicio debe ser una de las siguientes opciones: Desarrollo Web, Backend & API, Consultoría'),
  
+]
 
-    resultadoValidacion
+//para el post y put
+export const validacionServicio = [
+    ...reglasServicio.map((regla)=>regla.notEmpty().withMessage('Este campo es obligatorio')), resultadoValidacion
+]
+
+//para el patch
+export const validacionPatchServicio = [
+    ...reglasServicio.map((regla)=>regla.optional({values: 'falsy'})), resultadoValidacion
 ]
 
 export const validacionIDServicio = [
--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { prueba } from "../controllers/servicios.controllers.js";
 import { crearServicio, listarServicios, buscarServicioPorID, borrarServicioPorID, editarServicioPorID} from "../controllers/servicios.controllers.js"
-import { validacionIDServicio, validacionServicio } from "../middlewares/validacionServicios.js";
+import { validacionIDServicio, validacionServicio, validacionPatchServicio } from "../middlewares/validacionServicios.js";
 
 const router = Router();
 
 //http://localhost:3000/api/servicios/test
 router.route('/test').get(prueba)
 router.route('/').post(validacionServicio, crearServicio).get(listarServicios)
-router.route('/:id').get(validacionIDServicio, buscarServicioPorID).delete(validacionIDServicio, borrarServicioPorID).put([validacionIDServicio, validacionServicio], editarServicioPorID).patch(validacionIDServicio, editarServicioPorID)
+router.route('/:id').get(validacionIDServicio, buscarServicioPorID).delete(validacionIDServicio, borrarServicioPorID).put([validacionIDServicio, validacionServicio], editarServicioPorID).patch([validacionIDServicio, validacionPatchServicio], editarServicioPorID)
+
 export default router
